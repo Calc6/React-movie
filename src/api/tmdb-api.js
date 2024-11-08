5d3fee0532597f9cbaef75428bb0349ccc9c29c6
calc6 <calum.cullen66@gmail.com>
2024-11-08 22:53:57 +0000

    Added popularMovie file in comp

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -86,20 +86,3 @@ export const getMovie = (args) => {
       throw error
    });
   };
-
-  export const getPopMovie = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-    ).then((response) => {
-      if (!response.ok){
-        return response.json().then((error) => {
-          throw new Error(error.status_message || "Error");
-        });
-      }
-      return response.json();
-    }).catch((error) => {
-      throw error;
-    })
-  }
-  //Random comment github wont work 
-
